b6637f55a8cd667d713d8a5e1769f8ac840b14d4
Deenaz Fathima <fathimadeenaz@gmail.com>
2022-07-14 16:10:21 +0530

    added complete icon✅

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import { MdRadioButtonUnchecked } from "react-icons/md";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
@@ -23,10 +25,33 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
 			className={todo.isComplete ? "todo-row complete" : "todo-row"}
 			key={index}
 		>
-			<div key={todo.id} onClick={() => completeTodo(todo.id)}>
-				{todo.text}
+			<div id="group-1">
+				<div className="icons">
+					<MdRadioButtonUnchecked
+						onClick={() => completeTodo(todo.id)}
+						className={
+							!todo.isComplete
+								? "empty-icon"
+								: "empty-icon hidden"
+						}
+					/>
+					<IoMdCheckmarkCircleOutline
+						onClick={() => completeTodo(todo.id)}
+						className={
+							!todo.isComplete
+								? "complete-icon hidden"
+								: "complete-icon"
+						}
+					/>
+				</div>
+				<div
+					key={todo.id}
+					// onClick={() => completeTodo(todo.id)}
+				>
+					{todo.text}
+				</div>
 			</div>
-			<div className="icons">
+			<div className="icons" id="group-2">
 				<RiCloseCircleLine
 					onClick={() => removeTodo(todo.id)}
 					className="delete-icon"
